ca3ccaec1bb8501ec97895f18a9cfa0c95d50772
mvv <mvv@alc.es>
2018-09-16 08:53:46 +0200

    Creado nuevo proyecto test a la solucion. Creada primera prueba

---
+++ DojoApp.Test/GameOfLifeTest.cs
@@ -0,0 +1,25 @@
+﻿using DojoApp.Core.Entities;
+using DojoApp.Core.Katas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DojoApp.Test
+{
+    public class GameOfLifeTest
+    {
+        [Fact]
+        public void AnyLiveCellWithFewerThanTwoLiveCellsNeighbor_Dies()
+        {
+            GameOfLife game = new GameOfLife();
+
+            List<Coordinate2D> liveCellCoordinateList = new List<Coordinate2D>();
+            liveCellCoordinateList.Add(new Coordinate2D(2, 2));
+
+            var newLiveCellCoordinateLis = game.NextGeneration(liveCellCoordinateList);
+
+            Assert.Null(newLiveCellCoordinateLis.Find(c => c.Equals(2, 2)));
+        }
+    }
+}
